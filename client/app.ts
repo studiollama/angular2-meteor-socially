56270ffaa2f9edc507dfa982f3c669c3fc7469c7
Matthew Lahmann <lahmannm@gmail.com>
2016-03-22 11:40:40 -0700

    First commit

---
+++ client/app.ts
@@ -0,0 +1,21 @@
+﻿import 'reflect-metadata';
+import {NgZone, Component } from 'angular2/core';
+import { bootstrap } from 'angular2-meteor-auto-bootstrap'
+import {Parties} from '../collections/parties';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'client/app.html'
+}) 
+
+class App {
+    parties: Array<Object>;
+
+
+    constructor(zone: NgZone) {
+        Tracker.autorun(() => zone.run(() => {
+            this.parties = Parties.find().fetch();
+        }));
+    }
+}
+bootstrap(App);
---
+++ collections/parties.ts
@@ -0,0 +1,19 @@
+﻿export var Parties = new Mongo.Collection('parties');
+if (Meteor.isServer) {
+   
+
+    Parties.allow({
+        insert: function () {
+            var user = Meteor.user();
+            return !!user;
+        },
+        update: function () {
+            var user = Meteor.user();
+            return !!user;
+        },
+        remove: function () {
+            var user = Meteor.user();
+            return !!user;
+        }
+    });
+}
